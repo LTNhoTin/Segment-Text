16363feb94e0644fd61d6a7221e23dece5308b37
trung <trunglh113@gmail.com>
2025-03-01 20:39:56 +0700

    group chunk in chunking function

--- utils.py
+++ utils.py
@@ -5,23 +5,36 @@ import torch
 
 def chunking(model: SaT, sub_text: list[str], number: int = 128) -> list[str]:
     """
-    Split the given sub_text into chunks using the SaT model.
+    Splits the input text into smaller chunks based on the specified number of tokens.
 
     Args:
         model (SaT): The SaT model used for splitting the text.
-        sub_text (list[str]): List of text segments to be split.
-        number (int): Block size for splitting. Default is 128.
+        sub_text (list[str]): The list of strings to be chunked.
+        number (int, optional): The maximum number of tokens per chunk. Defaults to 128.
 
     Returns:
-        list[str]: List of split text segments.
+        list[str]: A list of chunked text strings.
     """
 
-    new_sub_text = model.split(sub_text,
-                               block_size=number,
-                               strip_whitespace=True,
-                               verbose=False)
-    new_sub_text = list(chain.from_iterable(list(new_sub_text)))
-    return list(new_sub_text)
+    sub_text = model.split(sub_text,
+                           block_size=256,
+                           strip_whitespace=True,
+                           verbose=False)
+    sub_text = list(chain.from_iterable(list(sub_text)))
+
+    new_sub_text = []
+    chunk = ""
+
+    for text in sub_text:
+        check_chunk = chunk + text
+        n_token = len(check_chunk.split())
+        if n_token > number:
+            new_sub_text.append(chunk)
+            chunk = text
+        else:
+            chunk += text
+
+    return new_sub_text
 
 def device():
     """
